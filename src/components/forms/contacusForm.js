20afc534738287f73a86e50f43a542c797a93cc6
niramnagrab <niramnagrab@gmail.com>
2022-04-03 21:32:17 +0800

    Connected form to Hubspot

--- src/components/forms/contacusForm.js
+++ src/components/forms/contacusForm.js
@@ -2,14 +2,50 @@ import React from "react"
 import Button from "../../layout/buttons"
 
 function ContactUs({ t, heading, buttonText }) {
+
+  const showMessage = (e) => {
+    e.preventDefault()
+    document.getElementById('Bene').value = ''
+    document.getElementById('Email').value = ''
+    document.getElementById('WeChatId').value = ''
+    document.getElementById('showMessage').innerHTML = t("registration_message")
+  }
+
   return (
-    <div className="contact-us-container">
-      <p>{t(heading)}</p>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Email" />
-      <input type="number" placeholder="Phone Number" />
-      <Button buttonText={buttonText} />
-    </div>
+    <form className="contact-us-container" onSubmit={(e) => showMessage(e)}>
+
+<p>{t(heading)}</p>
+       <input
+                  type="text"
+                  className="input"
+                  required
+                  id="Bene"
+                  name="Bene"
+                  placeholder={t('subscriber_name')}
+                />
+                <input
+                  type="email"
+                  className="input"
+                  required
+                  id="Email"
+                  name="Email"
+                  placeholder={t('subscriber_email')}
+                />
+                <input
+                  type="number"
+                  className="input"
+                  required
+                  id="WeChatId"
+                  name="WeChatId"
+                  placeholder={t('subscriber_number')}
+                />
+                <div className="button-container">
+                <button className="button" aria-label="Trial Class">
+                  {t('req_free_trail')}
+                </button></div>
+
+                <p className="text-success" id="showMessage"></p>
+    </form>
   )
 }
 export default ContactUs
